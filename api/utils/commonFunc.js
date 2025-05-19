c9436fb120f9b0276ce42b382686a7a349fc7adf
sm <sahil.malik@xpressioncorp.com>
2025-05-19 15:43:00 +0530

    otp-range updated!

--- api/utils/commonFunc.js
+++ api/utils/commonFunc.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 // Function to get random number
 const getRandomNumber = (start, end) => {
   try {
-    const randomNumber = Math.floor(start + Math.random() * end);
+    const randomNumber = Math.floor(Math.random() * (end - start + 1)) + start;
     return randomNumber;
   } catch (error) {
     throw error;
@@ -16,7 +16,17 @@ const generateToken = (data, expiresIn) => {
   return token;
 };
 
+const verifyToken = (token) => {
+  try {
+    const decoded = jwt.verify(token, process.env.TOKEN_SECRET);
+    return decoded;
+  } catch (error) {
+    throw error;
+  }
+};
+
 module.exports = {
   getRandomNumber,
   generateToken,
+  verifyToken,
 };
